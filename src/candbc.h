0ff60ed2161ed3a102c6c7cacbd9885493607895
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-06-12 14:22:40 +0200

    Regex - first blood
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- src/candbc.h
+++ src/candbc.h
@@ -1,9 +1,20 @@
 #ifndef __CANDBC_H
 #define __CANDBC_H
 
+#include <memory>
+
+namespace spdlog {
+    class logger;
+}
+
 class CANdbc
 {
-    CANdbc();
+public:
+    CANdbc(const std::shared_ptr<spdlog::logger> &logger);
+    bool load(const std::string &filename);
+
+private:
+    std::shared_ptr<spdlog::logger> mLogger;
 };
 
 #endif /* !__CANDBC_H */
--- tests/parsing.cpp
+++ tests/parsing.cpp
@@ -1,5 +1,30 @@
+#include <spdlog/spdlog.h>
+#include <candbc.h>
+
+#include <regex>
+#include <iostream>
+
 int main()
 {
+    spdlog::set_level(spdlog::level::debug);
+    spdlog::set_pattern("[%n][%L] %v");
+    auto log = spdlog::stdout_color_mt("CANdbc"); 
+    CANdbc dbc(log);
+
+    std::regex regTst("^BO\\_ (\\w+) (\\w+) *: (\\w+) (\\w+)");
+
+    std::string strTst("BO_ 2027 RawChannel11_IocTx: 8 IviBrd2");
+
+    std::smatch res;
+
+    if(std::regex_match(strTst, res, regTst)) {
+        log->info("Match! Size={}", res.size()-1);
+
+        for(unsigned long i = 1; i < res.size(); ++i) {
+            log->info("Match {}, str: {}", i, res[i].str());
+        }
+    }
+
     return 0;
 }
 
