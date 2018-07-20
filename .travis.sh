12416ce5d13fe8d05c4de90c315a80ba27ae5d38
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2018-07-20 14:11:59 +0200

    Fix permissions problem
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- .travis.sh
+++ .travis.sh
@@ -11,7 +11,7 @@ echo "ci_env: $ci_env"
 docker run \
      $ci_env -e WITH_COVERAGE -u cds -e CC -e CXX -e DISPLAY=:10 --privileged --cap-add=ALL \
      -v `pwd`:/home/sources rkollataj/candevstudio /bin/bash \
-     -c "cd /home/sources && rm -rf build && mkdir -p build && cd build &&
+     -c "sudo service xvfb start && sudo chown cds:cds /home/sources && cd /home/sources && rm -rf build && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
         $cmake_command &&
         make -j5 &&
         make test"
