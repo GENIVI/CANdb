07c807a6fc73f59ed8be3e5b1f6a2ec1c35af7fe
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2019-10-31 08:24:47 +0100

    Add CANDB__ prefix to CMAKE variables
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- .travis.sh
+++ .travis.sh
@@ -2,14 +2,14 @@
 
 cmake_command="cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE"
 
-if [ "$WITH_COVERAGE" == "ON" ]; then
-    cmake_command="$cmake_command -DWITH_COVERAGE=ON"
+if [ "$CANDB__WITH_COVERAGE" == "ON" ]; then
+    cmake_command="$cmake_command -DCANDB__WITH_COVERAGE=ON"
 fi
 
 echo "CMake: $cmake_command"
 echo "ci_env: $ci_env"
 docker run \
-     $ci_env -e WITH_COVERAGE -u cds -e CC -e CXX -e DISPLAY=:10 --privileged --cap-add=ALL \
+     $ci_env -e CANDB__WITH_COVERAGE -u cds -e CC -e CXX -e DISPLAY=:10 --privileged --cap-add=ALL \
      -v `pwd`:/home/sources rkollataj/candevstudio /bin/bash \
      -c "sudo service xvfb start && sudo chown -R cds:cds /home/sources && cd /home/sources && rm -rf build && mkdir -p build && cd build && source /opt/qt58/bin/qt58-env.sh &&
         $cmake_command &&
