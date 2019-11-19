9c58353ba5d6d4682f5dd919a0f34575a47c40e4
jenszo <webmaster@jenszo.de>
2019-11-19 01:13:35 +0100

    Updating global logging to match spdlog v1.4.2 API

--- tests/dbcparser_tests.cpp
+++ tests/dbcparser_tests.cpp
@@ -15,15 +15,7 @@ std::shared_ptr<spdlog::logger> kDefaultLogger
         logger->set_level(spdlog::level::err);
     } else {
         const std::string ll{ z };
-
-        auto it = std::find_if(std::begin(spdlog::level::level_names),
-            std::end(spdlog::level::level_names),
-            [&ll](const char* name) { return std::string{ name } == ll; });
-
-        if (it != std::end(spdlog::level::level_names)) {
-            int i = std::distance(std::begin(spdlog::level::level_names), it);
-            logger->set_level(static_cast<spdlog::level::level_enum>(i));
-        }
+        logger->set_level(spdlog::level::from_str(ll));
     }
 
     return logger;
