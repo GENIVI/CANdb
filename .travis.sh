b92f9031742d209948b18f501d4fa831523db93b
Jens Nachtigall <mailinglists@jenszo.de>
2020-02-18 14:07:05 +0100

    dumping coverage variable

--- .travis.sh
+++ .travis.sh
@@ -2,10 +2,6 @@
 
 cmake_command="cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=$(pwd)/install"
 
-if [ "$CANDB__WITH_COVERAGE" == "ON" ]; then
-    cmake_command="$cmake_command -DCANDB__WITH_COVERAGE=ON"
-fi
-
 echo "CMake: $cmake_command"
 echo "Working dir: $(pwd)"
 ls -la
