ec1977a459a37da557480cc9e85d351436598929
Bartosz Taczała <bartosz.taczala@siili.com>
2020-06-07 16:27:24 +0200

    Use std::filesystem

--- tests/test_helper.cpp
+++ tests/test_helper.cpp
@@ -1,15 +1,16 @@
 #include "test_helper.hpp"
 #include "spdlog/fmt/bundled/format.h"
 
+#include <filesystem>
 #include <fstream>
 #include <stdexcept>
 
 #include <spdlog/fmt/fmt.h>
 
 namespace test_helper {
-std::string loadDBCFile(const std::string& dbcFileName)
+std::string loadDBCFile(const std::filesystem::path& dbcFileName)
 {
-    const std::string path = std::string{ DBC_DIR } + dbcFileName;
+    const auto path = std::filesystem::path{ DBC_DIR } / dbcFileName;
 
     std::fstream ff{ path.c_str() };
 
@@ -20,7 +21,7 @@ std::string loadDBCFile(const std::string& dbcFileName)
     ff.close();
     if (buff.empty())
         throw std::runtime_error(
-            fmt::format("File {} does not exists", path));
+            fmt::format("File {} does not exists", path.string()));
     return buff;
 }
 } // namespace test_helper
--- tests/test_helper.hpp
+++ tests/test_helper.hpp
@@ -2,9 +2,10 @@
 #define TEST_HELPER_HPP_LRQG1DTL
 
 #include <string>
+#include <filesystem>
 
 namespace test_helper {
-std::string loadDBCFile(const std::string& dbcFileName);
+std::string loadDBCFile(const std::filesystem::path& dbcFileName);
 }
 
 #endif /* end of include guard: TEST_HELPER_HPP_LRQG1DTL */
