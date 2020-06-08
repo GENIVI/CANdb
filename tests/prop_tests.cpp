3fb1d566c5274444e2a4b7e9f431aec20d069e44
Bartosz Taczała <bartosz.taczala@siili.com>
2020-06-08 09:01:06 +0200

    Move kDefaultLogger to test_helper

--- tests/prop_tests.cpp
+++ tests/prop_tests.cpp
@@ -6,28 +6,6 @@
 #include "opendbc_tests_expected_data.hpp"
 #include "test_helper.hpp"
 
-std::shared_ptr<spdlog::logger> kDefaultLogger
-    = []() -> std::shared_ptr<spdlog::logger> {
-    auto z = std::getenv("CDB_LEVEL");
-    auto logger = spdlog::stdout_color_mt("cdb");
-
-    if (z == nullptr) {
-        logger->set_level(spdlog::level::err);
-    } else {
-        const std::string ll{ z };
-
-        auto it = std::find_if(std::begin(spdlog::level::level_names),
-            std::end(spdlog::level::level_names),
-            [&ll](const char* name) { return std::string{ name } == ll; });
-
-        if (it != std::end(spdlog::level::level_names)) {
-            int i = std::distance(std::begin(spdlog::level::level_names), it);
-            logger->set_level(static_cast<spdlog::level::level_enum>(i));
-        }
-    }
-
-    return logger;
-}();
 
 struct PropDBCTest : public ::testing::Test {
     CANdb::DBCParser parser;
--- tests/test_helper.cpp
+++ tests/test_helper.cpp
@@ -6,6 +6,30 @@
 #include <stdexcept>
 
 #include <spdlog/fmt/fmt.h>
+#include <spdlog/spdlog.h>
+
+std::shared_ptr<spdlog::logger> kDefaultLogger
+    = []() -> std::shared_ptr<spdlog::logger> {
+    auto z = std::getenv("CDB_LEVEL");
+    auto logger = spdlog::stdout_color_mt("cdb");
+
+    if (z == nullptr) {
+        logger->set_level(spdlog::level::err);
+    } else {
+        const std::string ll{ z };
+
+        auto it = std::find_if(std::begin(spdlog::level::level_names),
+            std::end(spdlog::level::level_names),
+            [&ll](const char* name) { return std::string{ name } == ll; });
+
+        if (it != std::end(spdlog::level::level_names)) {
+            int i = std::distance(std::begin(spdlog::level::level_names), it);
+            logger->set_level(static_cast<spdlog::level::level_enum>(i));
+        }
+    }
+
+    return logger;
+}();
 
 namespace test_helper {
 std::string loadDBCFile(const std::filesystem::path& dbcFileName)
