5c5d5d9289e2a091df6669de224805e5b7e46a65
Maximilian Güntner <code@klandest.in>
2020-08-02 16:39:16 +0200

    tests: add comfort.dbc file
    this file has been copied 1:1 from
    https://github.com/ANCIT/pythoncan-examples/blob/master/python-can-examples/resources/comfort.dbc
    
    and is  licensed under the EPL 2.0
    
    This dbc file seems to have been produced with a Vector tool
    and features EV_ (environment variables) and BA_DEF with FLOAT
    values. Both of which are missing in the peg parser grammar.

--- tests/prop_tests.cpp
+++ tests/prop_tests.cpp
@@ -15,3 +15,9 @@ TEST_F(PropDBCTest, extended_ids)
     const auto file = test_helper::loadDBCFile("prop/extended-ids.dbc");
     ASSERT_TRUE(parser.parse(file));
 }
+
+TEST_F(PropDBCTest, vector_file)
+{
+    const auto file = test_helper::loadDBCFile("prop/comfort.dbc");
+    ASSERT_TRUE(parser.parse(file));
+}
