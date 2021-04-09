d8e2b6d7b5f63937c0d16f8ca4eebf9e38ce3cab
Bartosz Taczała <bartosz.taczala@siili.com>
2021-04-09 13:50:58 +0200

    Revert "Fixing handling of muxed messages (#33)"
    This reverts commit 054c7ff4ee0b87770415abb41f69a9b019bd840e.

--- src/cantypes.hpp
+++ src/cantypes.hpp
@@ -8,15 +8,16 @@
 
 struct CANsignal {
 
+    // plain enums to retain backwards compatibility
     enum ByteOrder { Motorola = 0, Intel = 1 };
     enum SignType { Unsigned = false, Signed = true};
-    enum MuxIndex { /*mux slot 0 .. 253 */ MuxMaster = 0xFE, NonMuxed = 0xFF };
 
     // Constructor required for vs2015
     CANsignal(std::string _signal_name, std::uint8_t _startBit,
         std::uint8_t _signalSize, CANsignal::ByteOrder _byteOrder, CANsignal::SignType _valueSigned,
         float _factor, float _offset, float _min, float _max, std::string _unit,
-        std::vector<std::string> _receiver, std::uint8_t _muxNdx = NonMuxed)
+        std::vector<std::string> _receiver, const std::string& _mux = "",
+        std::uint8_t _muxNdx = 0)
         : signal_name(_signal_name)
         , startBit(_startBit)
         , signalSize(_signalSize)
@@ -28,6 +29,7 @@ struct CANsignal {
         , max(_max)
         , unit(_unit)
         , receiver(_receiver)
+        , mux(_mux)
         , muxNdx(_muxNdx)
     {
     }
@@ -43,7 +45,8 @@ struct CANsignal {
     float max;
     std::string unit;
     std::vector<std::string> receiver;
-    std::uint8_t muxNdx;
+    std::string mux = "";
+    std::uint8_t muxNdx{ 0 };
 
     bool operator==(const CANsignal& rhs) const
     {
@@ -53,27 +56,19 @@ struct CANsignal {
                (byteOrder == rhs.byteOrder) &&
                (valueSigned == rhs.valueSigned) &&
                (factor == rhs.factor) &&
-               (offset == rhs.offset) &&
-               (min == rhs.min) &&
-               (max == rhs.max) &&
-               (muxNdx == rhs.muxNdx);
-        // Theres is more to compare, yet this is the logical minimum without too much
-        // compromise on performance as the following arguably do not taint the signals
-        // structure
-        // (unit == rhs.unit)
-        // (receiver == rhs.receiver)
+               (offset == rhs.offset);
+        // Theres is more to compare, yet this is the logical minimum.
     }
 };
 
 struct CANmessage {
     // Constructor required for vs2015
     CANmessage(std::uint32_t _id, const std::string& _name = "",
-        std::uint32_t _dlc = 0, const std::string& _ecu = "", bool _muxed = false)
+        std::uint32_t _dlc = 0, const std::string& _ecu = "")
         : id(_id)
         , name(_name)
         , dlc(_dlc)
         , ecu(_ecu)
-        , muxed(_muxed)
     {
     }
 
@@ -81,7 +76,6 @@ struct CANmessage {
     std::string name;
     std::uint32_t dlc;
     std::string ecu;
-    bool muxed;
     std::uint32_t updateCycle{ 0 };
     std::string initValue{ "" };
 };
