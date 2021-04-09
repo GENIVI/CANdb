8c5a4c151e1f3001f136f9d500b37321a49dc238
Bartosz Taczała <bartosz.taczala@siili.com>
2021-04-09 13:38:35 +0200

    Enable building with conan

--- tools/dbclint/main.cpp
+++ tools/dbclint/main.cpp
@@ -1,15 +1,17 @@
 #include <cxxopts.hpp>
 #include <fstream>
 #include <regex>
-#include <spdlog/fmt/fmt.h>
 #include <system_error>
 
+#include <spdlog/fmt/fmt.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <termcolor/termcolor.hpp>
+#include <tl/expected.hpp>
+
 #include "dbcparser.h"
-#include "expected.hpp"
 #include "log.hpp"
 #include "parser.hpp"
 #include "parsererror.hpp"
-#include "termcolor.hpp"
 
 extern std::string dbc_grammar;
 
@@ -101,14 +103,7 @@ std::shared_ptr<spdlog::logger> kDefaultLogger = []() -> std::shared_ptr<spdlog:
         logger->set_level(spdlog::level::err);
     } else {
         const std::string ll{ z };
-
-        auto it = std::find_if(std::begin(spdlog::level::level_names), std::end(spdlog::level::level_names),
-            [&ll](const char* name) { return std::string{ name } == ll; });
-
-        if (it != std::end(spdlog::level::level_names)) {
-            int i = std::distance(std::begin(spdlog::level::level_names), it);
-            logger->set_level(static_cast<spdlog::level::level_enum>(i));
-        }
+        logger->set_level(spdlog::level::from_str(std::string{ z }));
     }
 
     return logger;
